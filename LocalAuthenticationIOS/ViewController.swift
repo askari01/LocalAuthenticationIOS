d01becb4207dd8678e66691137b8792269ba5f37
Farrukh Askari <farrukh.askari01@gmail.com>
2016-12-27 22:51:19 +0500

    initial commit

--- LocalAuthenticationIOS/ViewController.swift
+++ LocalAuthenticationIOS/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        authenticateUser()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,42 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func authenticateUser() {
+        print ("hello")
+        let context = LAContext()
+        
+        var error: NSError?
+        
+        let reasonString = "Authentication is needed to login"
+        
+        if (context.canEvaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, error: &error)) {
+            context .evaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, localizedReason: reasonString, reply: { (success, error) in
+                print ("result")
+                if (success) {
+                    print("Auth was OK")
+                    self.performSegue(withIdentifier: "Pass", sender: self)
+                }
+                else
+                {
+                    //You should do better handling of error here but I'm being lazy
+                    print("Error received: %d", error!)
+                }
+            })
+        } else{
+            // If the security policy cannot be evaluated then show a short message depending on the error.
+            
+            // Show the custom alert view to allow users to enter the password.
+            self.showPasswordAlert()
+        }
+    }
+    
+    func showPasswordAlert() {
+        let alertController = UIAlertController(title: "Auth Status", message: "Auth is not activated", preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "Close", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
 
 }
 
